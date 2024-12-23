7086e81aa605ec545b47ac95da16d7bcf0c11d97
Milligen van M. (Mika) <m.van.milligen@nedasco.nl>
2024-12-23 16:03:37 +0100

    Day 3

---
+++ day3/3a.go
@@ -0,0 +1,38 @@
+package day3
+
+import (
+	"aoc2024/lib"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func SolutionA() {
+	start := time.Now()
+	r, _ := regexp.Compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)")
+	replacer := strings.NewReplacer("mul(", "", ")", "")
+	list := [][]int{}
+
+	lib.ReadFileByLine("day3/input.txt", func(line string) {
+		matches := r.FindAllString(line, -1)
+		for _, match := range matches {
+			newString := replacer.Replace(match)
+			parts := strings.Split(newString, ",")
+			partOne, _ := strconv.Atoi(parts[0])
+			partTwo, _ := strconv.Atoi(parts[1])
+			ints := []int{partOne, partTwo}
+			list = append(list, ints)
+		}
+	})
+
+	sum := 0
+
+	for _, item := range list {
+		total := item[0] * item[1]
+		sum += total
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("Answer day 3a: %d in %s\n", sum, elapsed)
+}
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"aoc2024/day1"
 	"aoc2024/day2"
+	"aoc2024/day3"
 )
 
 func main() {
 	day1.SolutionB()
 	day2.SolutionA()
 	day2.SolutionB()
+	day3.SolutionA()
+	day3.SolutionB()
 }
